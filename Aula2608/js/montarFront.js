9b70bce45082f6931eb2dd131a31a3ce47396f72
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-26 09:30:29 -0300

    01 - Index pronto

---
+++ Aula2608/js/montarFront.js
@@ -0,0 +1,50 @@
+import { produtos } from "./objetos.js";
+
+// Confirmando que o HTML foi carregado
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Recuperando elementos do HTML - Para o JS
+    const elMain = document.querySelector('#gridProdutos')
+    const elSelectFiltro = document.querySelector('#filtroCategoria')
+    const elBtnBuscar = document.querySelector('#btnBuscar')
+    const elTxtBuscar = document.querySelector('#txtBuscar')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
